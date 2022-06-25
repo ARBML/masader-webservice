34f5f180cfc467c4041ee8481b342de34128825f
AliOsm <aliosm1997@gmail.com>
2022-06-25 10:08:22 +0300

    Add single dataset endpoint based on dataset index

--- app.py
+++ app.py
@@ -5,26 +5,35 @@ from datasets import DownloadMode, load_dataset
 
 app = Flask(__name__)
 
+
+def load_masader_dataset_as_dict():
+    return list(
+        load_dataset(
+            'arbml/masader',
+            download_mode=DownloadMode.FORCE_REDOWNLOAD,
+        )['train']
+    )
+
+
 print('Downloading the dataset...')
-masader = list(
-    load_dataset(
-        'arbml/masader',
-        download_mode=DownloadMode.FORCE_REDOWNLOAD,
-    )['train']
-)
+masader = load_masader_dataset_as_dict()
 
 
-@app.route('/datasets')
-def datasets():
+@app.route('/datasets', defaults={'index': None})
+@app.route('/datasets/<index>')
+def datasets(index: str):
     global masader
 
     if random.random() <= 0.1:
         print('Re-downloading the dataset...')
-        masader = list(
-            load_dataset(
-                'arbml/masader',
-                download_mode=DownloadMode.FORCE_REDOWNLOAD,
-            )['train']
-        )
+        masader = load_masader_dataset_as_dict()
 
-    return jsonify(masader)
+    if index:
+        index = int(index)
+
+        if 1 <= index <= len(masader):
+            return jsonify(masader[index - 1])
+        else:
+            return jsonify(f'Dataset index is out of range, the index should be between 1 and {len(masader)}.'), 404
+    else:
+        return jsonify(masader)
