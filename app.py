a2997596a69081f92cece01e163e1b26afe38c1d
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-06-26 19:56:20 +0100

    Completing dynamically retrieving embeddings and clusters information

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 import os
-import json
 import requests
 import pandas as pd
 from flask import Flask, jsonify
 from datasets import DownloadMode, load_dataset
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 API_TOKEN = os.environ['HF_SECRET_KEY']
-API_URL = "https://api-inference.huggingface.co/models/"
+API_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 
 def load_masader_dataset_as_dict():
-    return list(load_dataset(
+    return load_dataset(
         'arbml/masader',
         download_mode=DownloadMode.FORCE_REDOWNLOAD,
-    )['train'])
+    )['train']
 
 
 @app.route('/datasets', defaults={'index': None})
@@ -44,8 +43,9 @@ def refresh_datasets():
     global cluster
 
     print('Refreshing the dataset...')
-    masader = get_masader_dataset_as_dict()
-    embeddings = get_embeddings_data()
+    data = list(load_masader_dataset_as_dict())
+    masader = data
+    embeddings = get_embeddings_data(data)
     cluster = get_cluster_data()
 
     return jsonify(f'The datasets updated successfully! The current number of available datasets is {len(masader)}.')
@@ -59,8 +59,10 @@ def cluster_req():
 
 
 def get_cluster_data():
+    global tsne_data
+
     clustering = KMeans(n_clusters=15).fit(tsne_data)
-    return jsonify(pd.DataFrame(clustering.labels_.reshape(len(tsne_data), 1)).to_json('clusters.json', orient='split'))
+    return pd.DataFrame(clustering.labels_.reshape(len(tsne_data), 1)).to_json()
 
 
 # -----------Embeddings-------------
@@ -70,45 +72,57 @@ def embeddings_req():
     return embeddings
 
 
-def get_embeddings_data():
-    df = pd.DataFrame.from_dict(masader)
-    df.columns.values[0] = "No."
-    df.columns.values[1] = 'Name'
-    df_main_set = df[~df['No.'].isnull()]
-    df_main_set.loc[df_main_set['Abstract'].isnull(), 'Abstract'] = ""
+def get_embeddings_data(dataset):
+    desc = [desc if desc != 'nan' else "" for desc in dataset['Description']]
+    tasks = [tasks if tasks != 'nan' else "" for tasks in dataset['Tasks']]
+    abstracts = [abs if abs != 'nan' else "" for abs in dataset['Abstract']]
 
-    d_tasks = df_main_set[~df_main_set['Tasks'].isnull()]['Tasks']
-    d_tasks = [t for i, t in enumerate(d_tasks.values.tolist())]
+    full = [" ".join(tpl) for tpl in list(zip(*[desc, tasks, abstracts]))]
 
-    df_main_set.loc[df_main_set['Abstract'].isnull(), 'Abstract'] = df_main_set.loc[
-        df_main_set['Abstract'].isnull(), 'Description']
-    d_abstracts = df_main_set['Abstract']
-    d_abstracts = [d_tasks[i] + t.strip() for i, t in enumerate(d_abstracts.values.tolist())]
-
-    return embeddings_data(sentence_transformer_request(d_abstracts))
+    return embeddings_data(sentence_transformer_request(full))
 
 
 def sentence_transformer_request(d_abstracts):
-    data = json.dumps({
-        'sentences': d_abstracts,
-        'source_sentence': "",
-        'options': {'wait_for_model': True}
-    })
+    requested_data = None
+    first = 0
+    last = 0
 
-    response = requests.request("POST", API_URL + "sentence-transformers/all-MiniLM-L6-v2", headers=headers, data=data)
-    return json.loads(response.content.decode("utf-8"))
+    print(len(d_abstracts))
+
+    while last < len(d_abstracts):
+
+        if last + 200 < len(d_abstracts):
+            last += 200
+        else:
+            last += len(d_abstracts) - last
+
+        response = requests.post(API_URL + "sentence-transformers/all-MiniLM-L6-v2", headers=headers,
+                                 json={"inputs": d_abstracts[first:last], "options": {"wait_for_model": True}})
+
+        first = last
+
+        if requested_data is None:
+            requested_data = response.json()
+        else:
+            requested_data += response.json()
+
+    return pd.DataFrame(requested_data)
 
 
 def embeddings_data(abstract_embeddings):
+    global tsne_data
+
     model = TSNE(n_components=2, random_state=0)
     tsne_data = model.fit_transform(abstract_embeddings)
-    return jsonify(pd.DataFrame(tsne_data - tsne_data.min()))
+
+    return pd.DataFrame(tsne_data - tsne_data.min()).to_json()
 
 
 # ------------------------
 
-
 print('Downloading the dataset, embeddings, and clusters...')
-masader = load_masader_dataset_as_dict()
-embeddings = get_embeddings_data()
+tsne_data = None
+data = load_masader_dataset_as_dict()
+masader = list(data)
+embeddings = get_embeddings_data(data)
 cluster = get_cluster_data()
