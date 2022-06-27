6a61cb9afdbcdd2211d6c9ed00b93a6bafe7f42b
AliOsm <aliosm1997@gmail.com>
2022-06-27 17:17:22 +0300

    Add pagination and features filtration logic, Refactor some of the code

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
-from flask import Flask, jsonify
-from datasets import DownloadMode, load_dataset
+from flask import Flask, jsonify, request
+
+from utils import dict_filter, load_masader_dataset
+
 
 app = Flask(__name__)
 
 
-def load_masader_dataset_as_dict():
-    return list(
-        load_dataset(
-            'arbml/masader',
-            download_mode=DownloadMode.FORCE_REDOWNLOAD,
-        )['train']
-    )
-
-
 print('Downloading the dataset...')
-masader = load_masader_dataset_as_dict()
+masader = load_masader_dataset()
 
 
-@app.route('/datasets', defaults={'index': None})
-@app.route('/datasets/<index>')
-def datasets(index: str):
+@app.route('/datasets')
+def get_datasets():
     global masader
 
-    if index:
-        index = int(index)
+    page = request.args.get('page', default=1, type=int)
+    size = request.args.get('size', default=len(masader), type=int)
+    features = request.args.get('features', default='all', type=str).split(',')
 
-        if 1 <= index <= len(masader):
-            return jsonify(masader[index - 1])
-        else:
-            return jsonify(f'Dataset index is out of range, the index should be between 1 and {len(masader)}.'), 404
-    else:
-        return jsonify(masader)
+    masader_page = masader[(page - 1) * size : page * size]
+
+    if not masader_page:
+        return jsonify('Page not found.'), 404
+
+    return jsonify(list(map(lambda element: dict_filter(element, features), masader_page)))
+
+
+@app.route('/datasets/<int:index>')
+def get_dataset(index: int):
+    global masader
+
+    features = request.args.get('features', default='all', type=str).split(',')
+
+    if not (1 <= index <= len(masader)):
+        return jsonify(f'Dataset index is out of range, the index should be between 1 and {len(masader)}.'), 404
+
+    return jsonify(dict_filter(masader[index - 1], features))
 
 
 @app.route('/datasets/refresh')
@@ -38,6 +43,6 @@ def refresh_datasets():
     global masader
 
     print('Refreshing the dataset...')
-    masader = load_masader_dataset_as_dict()
+    masader = load_masader_dataset()
 
     return jsonify(f'The datasets updated successfully! The current number of available datasets is {len(masader)}.')
