7da96baef9c112d223abedc8b0abd541a0b0b4f5
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-06-30 21:32:14 +0100

    Small change

--- utils/hf_utils.py
+++ utils/hf_utils.py
@@ -7,7 +7,7 @@ from constants import HF_API_URL
 
 def request_hf_model(model_name: str, modelURL: str, data: List[str], hf_secret_key: str) -> requests.Response:
     return requests.post(
-        f'{HF_API_URL}{model_name}{modelURL}',
+        f'{HF_API_URL}{modelURL}{model_name}',
         headers={'Authorization': f'Bearer {hf_secret_key}'},
         json={
             'inputs': data,
