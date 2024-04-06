8f2f624f81665b66ec917f0b9db9e0f1fb56d64b
Zaid Alyafeai <alyafey22@gmail.com>
2024-04-06 15:40:43 +0300

    debug embeddings

--- utils/clusters_utils.py
+++ utils/clusters_utils.py
@@ -20,6 +20,8 @@ def compute_clusters(embeddings: List[List[float]]) -> List[int]:
 def compute_reduced_embeddings(embeddings: List[List[float]]) -> List[List[float]]:
     tsne_model = TSNE(n_components=2, random_state=42)
     for emb in embeddings:
+        if len(emb) != 384:
+            print(emb)
         assert len(emb) == 384
     embeddings = np.asarray(embeddings, dtype=object)
     print(embeddings.shape)
