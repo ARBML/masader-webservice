3f5580c09668720dc0b7bac2ad361db74fe2d608
zaidalyafeai <alyafey22@gmail.com>
2025-10-13 13:59:49 +0300

    switch to new hf inference point

--- utils/hf_utils.py
+++ utils/hf_utils.py
@@ -1,14 +1,16 @@
 from typing import List
 
 import requests
-
 from constants import HF_API_URL
 
 
 def request_hf_model(model_name: str, task: str, data: List[str], hf_secret_key: str) -> requests.Response:
     return requests.post(
-        f'{HF_API_URL}/pipeline/{task}/{model_name}',
-        headers={'Authorization': f'Bearer {hf_secret_key}'},
+        f"https://router.huggingface.co/hf-inference/models/{model_name}/pipeline/{task}",
+        headers={
+            'Authorization': f'Bearer {hf_secret_key}',
+            'Content-Type': 'application/json',
+        },
         json={
             'inputs': data,
             'options': {
